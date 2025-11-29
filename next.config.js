3c2a262faffd1b0f3b1abc8024f7a37dea559f3d
Samuel Addis <samueladdis@MacBookPro.localdomain>
2025-11-29 17:29:57 -0500

    rewrite to calendar app

--- next.config.js
+++ next.config.js
@@ -13,6 +13,43 @@ const nextConfig = {
       bodySizeLimit: "2mb",
     },
   },
+  async rewrites() {
+    const calendarApp = process.env.ROUTE_CALENDAR_APP;
+    return [
+      {
+        source: "/auth",
+        destination: `${calendarApp}/auth`,
+      },
+      {
+        source: "/auth/:path*",
+        destination: `${calendarApp}/auth/:path*`,
+      },
+      {
+        source: "/calendar",
+        destination: `${calendarApp}/calendar`,
+      },
+      {
+        source: "/calendar/:path*",
+        destination: `${calendarApp}/calendar/:path*`,
+      },
+      {
+        source: "/dashboard",
+        destination: `${calendarApp}/dashboard`,
+      },
+      {
+        source: "/dashboard/:path*",
+        destination: `${calendarApp}/dashboard/:path*`,
+      },
+      {
+        source: "/public",
+        destination: `${calendarApp}/public`,
+      },
+      {
+        source: "/public/:path*",
+        destination: `${calendarApp}/public/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
